094670a513c6546fdd949d0892749607537fa231
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-10-01 17:23:43 +0400

    Update 01_Trees.java

--- 01_Trees.java
+++ 01_Trees.java
@@ -123,30 +123,4 @@ class SimpleTree<T> {
             }
         return allLeafCount;
     } 
-
-    public static <T> void determinateLevels (SimpleTree<T> simpleTree) {
-		
-		if (simpleTree.Root == null) return;
-		
-		simpleTree.Root.Level = 0; 
-		
-		determinateLevels(simpleTree.Root.Children, 0, 1);
-		
-	}
-	
-	private static <T> void determinateLevels (List<SimpleTreeNode<T>> simpleTreeNodes, int index, int level) {
-
-		
-		if (simpleTreeNodes == null) return;
-		
-		if(index < simpleTreeNodes.size()) {
-			
-			simpleTreeNodes.get(index).Level = level;
-			
-			determinateLevels(simpleTreeNodes.get(index).Children, 0, level + 1);
-			determinateLevels(simpleTreeNodes, index + 1, level);
-				
-		}
-		
-	}
 }
